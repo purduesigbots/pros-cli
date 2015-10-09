8067bc08c278ab821da688b59f769404f50544d6
Elliot Berman <edjubuh@elliotjb.com>
2015-10-09 00:19:21 -0400

    Initial commit

---
+++ src/main/java/edu/purdue/sigbots/purdueros/cli/App.java
@@ -0,0 +1,13 @@
+package edu.purdue.sigbots.purdueros.cli;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
