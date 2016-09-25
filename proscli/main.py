85123f01f1e63b4fc7688e13104ee59c6efb263a
Elliot Berman <elliot@elliotjb.com>
2016-09-25 18:33:05 -0400

    Remove deprecated and broken pros help option

--- proscli/main.py
+++ proscli/main.py
@@ -12,21 +12,12 @@ def main():
         click.echo('Aborted!')
         pass
 
-import prosconductor.providers.utils
-
-@proscli.flasher_cli.command('help', short_help='Show this message and exit.')
-@click.argument('ignore', nargs=-1, expose_value=False)
-@default_options
-@click.pass_context
-def help_cmd(ctx):
-    click.echo(prosconductor.providers.utils.get_all_available_templates())
-
 
 @click.command('pros',
                cls=click.CommandCollection,
                context_settings=dict(help_option_names=['-h', '--help']),
                sources=[proscli.terminal_cli, proscli.build_cli, proscli.flasher_cli, proscli.conductor_cli])
-@click.version_option(version='2.1.923', prog_name='pros')
+@click.version_option(version='2.1.925', prog_name='pros')
 @default_options
 def cli():
     pass
