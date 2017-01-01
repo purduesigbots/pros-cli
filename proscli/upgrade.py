d662b4c5f08f6a27d370805f31bad98e2d40f931
Elliot Berman <edjubuh@gmail.com>
2016-12-31 20:12:58 -0500

    Add first iteration of upgrade command

---
+++ proscli/upgrade.py
@@ -0,0 +1,45 @@
+import click
+from proscli.utils import default_cfg
+import os
+import os.path
+import subprocess
+import sys
+
+import json
+
+@click.group()
+def upgrade_cli():
+    pass
+
+
+def get_upgrade_command():
+    if getattr(sys, 'frozen', False):
+        cmd = os.path.abspath(os.path.join(sys.executable, '..', '..', 'updater.exe'))
+        if os.path.exists(cmd):
+            return [cmd, '/silentall', '-nofreqcheck']
+        else:
+            return False
+    else:
+        try:
+            from pip._vendor import pkg_resources
+            results = [p for p in pkg_resources.working_set if p.project_name == 'pros-cli']
+            if len(results) == 0 or not hasattr(results[0], 'location'):
+                return False
+            else:
+                return 'pip install -U -d {} pros-cli'.format(results[0].location).split(' ')
+        except Exception:
+            return False
+
+
+@upgrade_cli.command('upgrade', help='Provides a facility to run upgrade the PROS CLI')
+@default_cfg
+def upgrade(cfg):
+    cmd = get_upgrade_command()
+    if cmd is False:
+        click.echo('Could not determine installation type.')
+        sys.exit(1)
+        return
+    elif not cfg.machine_output:
+        sys.exit(subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE))
+    else:
+        click.echo(' '.join(cmd))
