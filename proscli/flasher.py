f93a7632a6986cd323c920dcc71ee6f716a35938
Alex Brooke <brookea@purdue.edu>
2017-04-04 17:52:46 -0400

    finally stop flasher from printing retrying every time

--- proscli/flasher.py
+++ proscli/flasher.py
@@ -93,11 +93,10 @@ def flash(ctx, save_file_system, y, port, binary, no_poll, retry):
     click.echo('Flashing ' + binary + ' to ' + ', '.join(port))
     for p in port:
         tries = 1
-        while tries <= retry:
-            code = prosflasher.upload.upload(p, y, binary, no_poll, ctx)
-            if code or code == -1000:
-                break
+        code = prosflasher.upload.upload(p, y, binary, no_poll, ctx)
+        while tries <= retry and (not code or code == -1000):
             click.echo('Retrying...')
+            code = prosflasher.upload.upload(p, y, binary, no_poll, ctx)
             tries += 1
 
 
