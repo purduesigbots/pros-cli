6db2b047f62b5793c6f46a7ca5b4a8721e1aa968
Elliot Berman <berman5@purdue.edu>
2018-02-26 10:35:03 -0500

    Fix return Tuple if Blah else Tuple in DirectPort
    Summary: - Finally getting it right
    
    Test Plan: [X] Terminal and flashing crash a little less now
    
    Reviewers: O3 The Q Continuum, brookea
    
    Reviewed By: O3 The Q Continuum, brookea
    
    Subscribers: brookea
    
    Tags: #zorp
    
    Differential Revision: http://phabricator.purduesigbots.com/D51

--- pros/serial/ports/direct_port.py
+++ pros/serial/ports/direct_port.py
@@ -36,7 +36,7 @@ class DirectPort(BasePort):
                 msg, self.temp_buffer = self.temp_buffer.split(b'\0', 1)
                 msg = cobs.decode(msg)
                 self.buffer.append((msg[:4], msg[4:]))
-        return self.buffer.pop(0) if len(self.buffer) > 0 else b'', b''
+        return self.buffer.pop(0) if len(self.buffer) > 0 else (b'', b'')
 
     def _raw_read(self, n_bytes: int = 0):
         assert isinstance(self.buffer, bytearray)
@@ -50,7 +50,7 @@ class DirectPort(BasePort):
                 self.buffer.extend(self.serial.read(n_bytes - len(self.buffer)))
             msg = bytes(self.buffer[:n_bytes])
             self.buffer = self.buffer[n_bytes:]
-            return b'', msg if len(msg) > 0 else b'', b''
+            return (b'', msg) if len(msg) > 0 else (b'', b'')
 
     def read(self, n_bytes: int = 0):
         return self.decoder(n_bytes)
