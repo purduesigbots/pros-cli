b7f6a67d8a93c892f5aa5698978166c73900d262
Alex Medoff <amedoff@purdue.edu>
2018-03-03 16:28:01 -0500

    Inserting literal shit

--- pros/cli/jinx.py
+++ pros/cli/jinx.py
@@ -5,6 +5,8 @@ import pros.serial.ports as ports
 import pros.serial.vex.v5_device as v5_device
 from .common import PROSGroup, default_options
 
+def convert(bytestring):
+	return b''.join(reversed([bytestring[i:i+2] for i in range(0,len(bytestring),2)]))
 
 @click.group(cls=PROSGroup)
 def jinx_cli():
@@ -33,5 +35,21 @@ def jinx():
             if msg[0] == b'S'[0]:
                 click.echo(msg.hex())
                 click.echo(msgpack.unpackb(msg[1:]))
-            else:
-                click.echo(msg.hex())
+            elif msg[0] == b'D'[0]:
+				packet = {}
+				packet['type'] = msg[0]
+				packet['timestamp'] = convert(msg[1:5])
+				packet['data'] = []
+				i = 5
+				while i < len(msg):
+					packet['data'].append({})
+					id = convert(msg[i+3:i+7])
+					packet['data'][-1][id] = []
+					packet['data'][-1][id].append({})
+					packet['data'][-1][id][-1]['size'] = msg[i:i+2]
+					i += 2
+					packet['data'][-1][id][-1]['offset'] = msg[i:i+2]
+					i += 6
+					packet['data'][-1][id][-1]['value'] = msg[i:i + int(packet['data'][-1][id][-1]['size']) * 2]
+					i += int(packet['data'][-1][id][-1]['size']) * 2
+				click.echo(packet)
