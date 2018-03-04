4a839bf005ca5d6761bfee968a85ed00a2c690da
Alex Brooke <akb.sbc@gmail.com>
2018-03-04 04:42:55 -0500

    Added beginning of framework for the JINX server
    the server is meant to pass messages through from the connected microcontroller to our own FreeBoard-based frontend client after some filtering.
    
    Note that this commit adds websockets (a python package) to requirements.txt, so it will be necessary to reinstall the requirements after this commit.
    
    currently running into issues where 1) the server crashes after receiving some malformed message and 2) the server is no longer properly passing messages to the client.
    
    I'm pretty sure that (2) will be solved once (1) is, though

--- pros/cli/jinx.py
+++ pros/cli/jinx.py
@@ -1,16 +1,108 @@
+import asyncio
 import click
 import msgpack
+import json
+import struct
+from typing import Any, Dict
+import websockets
 
 import pros.serial.ports as ports
 import pros.serial.vex.v5_device as v5_device
 from .common import PROSGroup, default_options
 
-def convert(bytestring):
-	return b''.join(reversed([bytestring[i:i+2] for i in range(0,len(bytestring),2)]))
+
+DATA_HISTORY_MAX_COUNT = 500
+schemas = dict()
+unknowns = list()
+ser: ports.DirectPort = None
+
+
+def convert(b: str) -> str:
+    return ''.join(reversed([b[i:i+2] for i in range(0, len(b), 2)]))
+
 
 @click.group(cls=PROSGroup)
 def jinx_cli():
     pass
+
+
+def create_packet(msg: bytes) -> Dict[str, Any]:
+    """
+    create a packet for the JINX frontend
+    :param msg: message read from serial line
+    :return: packet of the form
+            {
+                'field_name': {
+                    'timestamp': List[int],
+                    'values': List[Union[int, chr, str, Dict[str, Any]]]
+                },
+            }
+            timestamps and values are kept as rolling lists in case value history needs to be accessed at any time
+    """
+    click.echo('building packet')
+    packet = dict()
+    # packet['type'] = int(msg.hex()[0:2], 16)
+    timestamp = int(convert(msg.hex()[2:10]), 16)
+    i = 10
+    while i < len(msg.hex()):
+        click.echo(f'{i} / {len(msg.hex())}: looping {msg.hex()}')
+        msg_id = int(convert(msg.hex()[i + 4:i + 8]), 16)
+        vsize = int(msg.hex()[i:i + 2], 16)
+        if msg_id in schemas.keys():
+            packet[schemas[msg_id][b'n']] = dict()
+            i += 2
+            click.echo('+2')
+            packet[schemas[msg_id][b'n']]['timestamp'] = timestamp + int(msg.hex()[i:i + 2], 16)
+            i += 6
+            click.echo('+6')
+            packet[schemas[msg_id][b'n']]['value'] = dict()
+            x = bytearray.fromhex(msg.hex()[i:i + vsize * 2])
+            y = struct.unpack(schemas[msg_id][b's'], x)
+            if 'values' not in packet[schemas[msg_id][b'n']].keys():
+                packet[schemas[msg_id][b'n']]['values'] = list()
+            # roll list over
+            packet[schemas[msg_id][b'n']]['values'] = packet[schemas[msg_id][b'n']]['values'][:DATA_HISTORY_MAX_COUNT]
+            if len(y) > 1:
+                v = dict()
+                for j in range(len(y)):
+                    k = schemas[msg_id][b'e'][j]
+                    k = str(k, 'ascii') if isinstance(k, bytes) else k
+                    v[k] = str(y[j], 'ascii') if isinstance(y[j], bytes) else y[j]
+            else:
+                v = str(y[j], 'ascii') if isinstance(y[j], bytes) else y[j]
+            packet[schemas[msg_id][b'n']]['values'].append(v)
+            i += vsize * 2
+            click.echo(f'+{vsize * 2}')
+        else:
+            # 8 + vsize*2 = 2 + 2 + 4 + 2*size = len(size) + len(offset) + 2 * size [in bits]
+            unknowns.append(msg[i:i + 8 + vsize * 2])
+            i += 8 + vsize * 2
+            click.echo(f'+14+{vsize}*2 ({8+vsize*2})')
+    click.echo(packet)
+    return packet
+
+
+async def poll_and_send_data(websocket, path):
+    click.echo('in poll')
+    while True:
+        stream, msg = ser.read()
+        # click.echo(str(stream, 'ascii'))
+        # click.echo(msg.hex())
+        if stream == b'jinx':
+            if msg[0] == b'S'[0]:
+                # click.echo(msg.hex())
+                schema = msgpack.unpackb(bytearray.fromhex(msg.hex()[2:]))
+                # click.echo(msgpack.unpackb(bytearray.fromhex(msg.hex()[2:])))
+                schemas.update(schema)
+                for msg in unknowns:
+                    packet = create_packet(msg)
+                    click.echo(json.dumps(packet))
+                    await websocket.send(json.dumps(packet))
+            elif msg[0] == b'D'[0]:
+                packet = create_packet(msg)
+                # click.echo(packet)
+                click.echo(json.dumps(packet))
+                await websocket.send(json.dumps(packet))
 
 
 @jinx_cli.command(short_help='Run JINX, the graphical debugger for PROS')
@@ -21,6 +113,7 @@ def jinx():
 
     Visit https://pros.cs.purdue.edu/v5/cli/jinx to learn more
     """
+    global ser
     _ports = [p.device for p in v5_device.find_v5_ports('user')]
     if len(_ports) > 1:
         port = click.prompt('Multiple V5 ports were found. Please pick one.',
@@ -29,27 +122,10 @@ def jinx():
         port = _ports[0]
     ser = ports.DirectPort(port)
     ser.config('cobs', True)
-    while True:
-        stream, msg = ser.read()
-        if stream == b'jinx':
-            if msg[0] == b'S'[0]:
-                click.echo(msg.hex())
-                click.echo(msgpack.unpackb(msg[1:]))
-            elif msg[0] == b'D'[0]:
-				packet = {}
-				packet['type'] = msg[0]
-				packet['timestamp'] = convert(msg[1:5])
-				packet['data'] = []
-				i = 5
-				while i < len(msg):
-					packet['data'].append({})
-					id = convert(msg[i+3:i+7])
-					packet['data'][-1][id] = []
-					packet['data'][-1][id].append({})
-					packet['data'][-1][id][-1]['size'] = msg[i:i+2]
-					i += 2
-					packet['data'][-1][id][-1]['offset'] = msg[i:i+2]
-					i += 6
-					packet['data'][-1][id][-1]['value'] = msg[i:i + int(packet['data'][-1][id][-1]['size']) * 2]
-					i += int(packet['data'][-1][id][-1]['size']) * 2
-				click.echo(packet)
+    click.echo('starting server')
+    loop = asyncio.get_event_loop()
+    start_server = websockets.serve(poll_and_send_data, 'localhost', 9001)
+    loop.run_until_complete(start_server)
+    loop.run_forever()
+    click.echo('after server')
+
