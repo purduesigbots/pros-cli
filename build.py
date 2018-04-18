1f1a1f5ae00c9223ab46ed1963a067f9ed719538
Elliot Berman <berman5@purdue.edu>
2018-04-17 23:23:04 -0400

    💚 Update build.py to work with pip 10.0.0
    Summary:
    https://stackoverflow.com/a/49867265/3175586
    
    Test Plan:
    [X] Pray it compiles
    [X] It builds in Jenkins now
    
    Reviewers: O3 The Q Continuum, brookea
    
    Reviewed By: O3 The Q Continuum, brookea
    
    Subscribers: brookea
    
    Tags: #zorp
    
    Differential Revision: http://phabricator.purduesigbots.com/D90

--- build.py
+++ build.py
@@ -2,7 +2,11 @@ import sys
 
 import requests.certs
 from cx_Freeze import Executable, setup
-from pip.req import parse_requirements
+
+try:  # for pip >= 10 -- https://stackoverflow.com/a/49867265/3175586
+    from pip._internal.req import parse_requirements
+except ImportError:  # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
 import pros
 
