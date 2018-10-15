0a9d8e523b7f253494cb29abe868e532dcd438f5
Elliot Berman <berman5@purdue.edu>
2018-10-15 15:46:25 -0400

    Add support for collapsing containers

--- pros/common/ui/interactive/components/container.py
+++ pros/common/ui/interactive/components/container.py
@@ -1,3 +1,4 @@
+from pros.common.ui.interactive.parameters import Parameter
 from typing import *
 
 from .component import Component
@@ -7,14 +8,19 @@ class Container(Component):
     """
     A Container has multiple Components, possibly a title, and possibly a description
     """
+
     def __init__(self, *elements: Component,
-                 title: Optional[AnyStr] = None, description: Optional[AnyStr] = None):
+                 title: Optional[AnyStr] = None, description: Optional[AnyStr] = None, collapsed: bool = False):
         self.title = title
         self.description = description
         self.elements = elements
+        self.collapsed = Parameter(collapsed)
 
     def __getstate__(self):
-        extra_state = {}
+        extra_state = {
+            'uuid': self.collapsed.uuid,
+            'collapsed': self.collapsed.value
+        }
         if self.title is not None:
             extra_state['title'] = self.title
         if self.description is not None:
