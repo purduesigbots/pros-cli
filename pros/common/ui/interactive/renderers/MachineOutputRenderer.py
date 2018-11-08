3b937c330e657c24259bef06260fc5cf1cb9bc9e
Elliot Berman <berman5@purdue.edu>
2018-11-07 19:56:21 -0500

    MachineOutputRenderer respects nesting, multithreading better

--- pros/common/ui/interactive/renderers/MachineOutputRenderer.py
+++ pros/common/ui/interactive/renderers/MachineOutputRenderer.py
@@ -1,18 +1,46 @@
 import json
-import sys
+from threading import current_thread, Semaphore
+
+import click
+from typing import *
 
 from pros.common import ui
 from pros.common.ui.interactive.observable import Observable
-from typing import *
-
 from .Renderer import Renderer
 from ..application import Application
+
+current: List['MachineOutputRenderer'] = []
+
+
+def _push_renderer(renderer: 'MachineOutputRenderer'):
+    global current
+
+    stack: List['MachineOutputRenderer'] = current
+    stack.append(renderer)
+
+
+def _remove_renderer(renderer: 'MachineOutputRenderer'):
+    global current
+
+    stack: List['MachineOutputRenderer'] = current
+    stack.remove(renderer)
+
+
+def _current_renderer() -> 'MachineOutputRenderer':
+    global current
+
+    stack: List['MachineOutputRenderer'] = current
+    return stack[-1]
 
 
 class MachineOutputRenderer(Renderer):
     def __init__(self, app: Application):
+        global current
+
         super().__init__(app)
         self.alive = False
+        self.thread = None
+        self.stop_sem = Semaphore(0)
 
         @app.on_redraw
         def on_redraw():
@@ -20,30 +48,61 @@ class MachineOutputRenderer(Renderer):
 
         app.on_exit(lambda: self.stop())
 
+    @staticmethod
+    def get_line():
+        line = click.get_text_stream('stdin').readline().strip()
+        return line.strip() if line is not None else None
+
     def run(self) -> Any:
+        _push_renderer(self)
+        self.thread = current_thread()
         self.alive = True
         while self.alive:
             self.render(self.app)
             if not self.alive:
                 break
-            line = sys.stdin.readline()
-            if line.strip().isspace():
+
+            line = self.get_line()
+            if not self.alive or not line or line.isspace():
                 continue
+
             try:
-                value = json.loads(line.strip())
+                value = json.loads(line)
                 if 'uuid' in value and 'event' in value:
                     Observable.notify(value['uuid'], value['event'], *value.get('args', []), **value.get('kwargs', {}))
             except json.JSONDecodeError as e:
                 ui.logger(__name__).exception(e)
+        self.stop_sem.release()
+        self.stop()
         return self.run_rv
 
     def stop(self):
+        ui.logger(__name__).debug(f'Stopping {self.app}')
+        self.alive = False
+
+        if current_thread() != self.thread:
+            ui.logger(__name__).debug(f'Interrupting render thread of {self.app}')
+            while not self.stop_sem.acquire(timeout=0.1):
+                self.wake_me()
+
+        ui.logger(__name__).debug(f'Broadcasting stop {self.app}')
         self._output({
             'uuid': self.app.uuid,
             'should_exit': True
         })
-        self.alive = False
-        ui.logger(__name__).info('All done!')
+
+        top_renderer = _current_renderer()
+        top_renderer.wake_me()
+
+    def wake_me(self):
+        """
+        Hack to wake up input thread to know to shut down
+        """
+        ui.logger(__name__).debug(f'Broadcasting WAKEME for {self.app}')
+        if ui.ismachineoutput():
+            ui._machineoutput({'type': 'wakeme'})
+        else:
+            ui.echo('Wake up the renderer!')
 
     @staticmethod
     def _output(data: dict):
