79f7d5bb14f243fcec553e93512afaff7e88bdfd
Alex Brooke <akb.sbc@gmail.com>
2021-01-24 15:51:36 -0500

    Update pros/cli/common.py
    Co-authored-by: nathan-moore <nathanmoore424@gmail.com>

--- pros/cli/common.py
+++ pros/cli/common.py
@@ -217,7 +217,7 @@ def resolve_v5_port(port: Optional[str], type: str, quiet: bool = False) -> Tupl
                 return None, False
         else:
             port = ports[0].device
-            is_joystick = type == 'user' and  'Controller' in ports[0].description
+            is_joystick = type == 'user' and 'Controller' in ports[0].description
             logger(__name__).info('Automatically selected {}'.format(port))
     return port, is_joystick
 
