24456c6cf88aebbde9c21028f97be38259a14bca
Alex Brooke <akb.sbc@gmail.com>
2021-01-24 15:21:15 -0500

    🔧 rename dist pros-cli-v5 -> pros-cli (#147)
    * 🔧 rename dist pros-cli-v5 -> pros-cli
    
    * use more declarative setup.py syntax
    
    * update dist name in build.py also

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 from install_requires import install_requires as install_reqs
 
 setup(
-    name='pros-cli-v5',
+    name='pros-cli',
     version=open('pip_version').read().strip(),
     packages=find_packages(),
     url='https://github.com/purduesigbots/pros-cli',
@@ -13,8 +13,10 @@ setup(
     author_email='pros_development@cs.purdue.edu',
     description='Command Line Interface for managing PROS projects',
     install_requires=install_reqs,
-    entry_points="""
-        [console_scripts]
-        prosv5=pros.cli.main:main
-        """
+    entry_points={
+        'console_scripts': [
+            'pros=pros.cli.main:main',
+            'prosv5=pros.cli.main:main'
+        ]
+    }
 )
