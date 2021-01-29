5fb4e45ef8f23c748ce1fba94fc34d6446d5b096
WillXuCodes <xu.z.william@gmail.com>
2021-01-29 12:00:53 -0800

    Added tolerance for schemas larger than the current length (future proofing for future VEXos updates)

--- pros/serial/devices/vex/v5_device.py
+++ pros/serial/devices/vex/v5_device.py
@@ -904,8 +904,10 @@ class V5Device(VEXDevice, SystemDevice):
             msg = msg[1:]
         if len(msg) > 0:
             logger(cls).debug('Set msg window to {}'.format(bytes_to_str(msg)))
-        if len(msg) != rx_length and check_length:
-            raise VEXCommError("Received length doesn't match {} (got {})".format(rx_length, len(msg)), msg)
+        if len(msg) < rx_length and check_length:
+            raise VEXCommError("Received length is less than {} (got {})".format(rx_length, len(msg)), msg)
+        elif len(msg) > rx_length and check_length:
+            ui.echo("WARNING: Recieved length is more than {} (got {}). Consider upgrading the PROS (CLI Version: {}).".format(rx_length, len(msg),self.query_system_version().system_version))
         return msg
 
     def _txrx_ext_packet(self, command: int, tx_data: Union[Iterable, bytes, bytearray],
