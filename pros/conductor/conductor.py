1d9f6bae72b7e44ca4329d337b3921b7c41ae6df
omegaStag <57331513+omegaStag@users.noreply.github.com>
2021-10-28 21:51:56 -0400

    Added download-ok switch
    Will elaborate in PR

--- pros/conductor/conductor.py
+++ pros/conductor/conductor.py
@@ -97,11 +97,13 @@ class Conductor(Config):
             shutil.rmtree(template.location)
         self.save()
 
-    def resolve_templates(self, identifier: Union[str, BaseTemplate], allow_online: bool = True,
+    def resolve_templates(self, identifier: Union[str, BaseTemplate], 
                           allow_offline: bool = True, force_refresh: bool = False,
                           unique: bool = True, **kwargs) -> List[BaseTemplate]:
         results = list() if not unique else set()
         kernel_version = kwargs.get('kernel_version', None)
+        allow_online = kwargs.get('allow_online', True)
+        download_ok = kwargs.get('download_ok', True)
         if isinstance(identifier, str):
             query = BaseTemplate.create_query(name=identifier, **kwargs)
         else:
@@ -112,7 +114,7 @@ class Conductor(Config):
                 results.update(offline_results)
             else:
                 results.extend(offline_results)
-        if allow_online:
+        if allow_online and download_ok:
             for depot in self.depots.values():
                 online_results = filter(lambda t: t.satisfies(query, kernel_version=kernel_version),
                                         depot.get_remote_templates(force_check=force_refresh, **kwargs))
@@ -173,7 +175,7 @@ class Conductor(Config):
         if 'kernel' in project.templates:
             # support_kernels for backwards compatibility, but kernel_version should be getting most of the exposure
             kwargs['kernel_version'] = kwargs['supported_kernels'] = project.templates['kernel'].version
-        template = self.resolve_template(identifier=identifier, allow_online=download_ok, **kwargs)
+        template = self.resolve_template(identifier=identifier, **kwargs)
         if template is None:
             raise dont_send(
                 InvalidTemplateException(f'Could not find a template satisfying {identifier} for {project.target}'))
