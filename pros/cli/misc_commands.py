90a157d6a33a87f758e4d00c36bbc2d72437c369
Benjamin Davis <davi1561@purdue.edu>
2022-04-07 18:47:18 -0400

    Add commands to read and set kernel variables

--- pros/cli/misc_commands.py
+++ pros/cli/misc_commands.py
@@ -3,6 +3,14 @@ from pros.cli.common import *
 from pros.ga.analytics import analytics
 
 @pros_root
+def set_variable():
+    pass
+
+@pros_root
+def read_variable():
+    pass
+
+@pros_root
 def misc_commands_cli():
     pass
 
@@ -35,3 +43,36 @@ def upgrade(force_check, no_install):
                 ui.logger(__name__).error(f'This manifest cannot perform the upgrade.')
                 return -3
             ui.finalize('upgradeComplete', manager.perform_upgrade())
+
+@set_variable.command(aliases=['sv'])
+@default_options
+@click.argument('variable', type=click.Choice(['teamnumber', 'robotname']), required=True)
+@click.argument('value', required=True, type=click.STRING, nargs=1)
+def set_variable(variable, value):
+    import pros.serial.devices.vex as vex
+    from pros.serial.ports import DirectPort
+
+    # Get the connected v5 device
+    port = resolve_v5_port(None, 'system')[0]
+    if port == None:
+        return
+    device = vex.V5Device(DirectPort(port))
+    device.kv_write(variable, value)
+    ui.finalize('setVariable', f'{variable} set to {value}')
+
+@read_variable.command(aliases=['rv'])
+@default_options
+@click.argument('variable', type=click.Choice(['teamnumber', 'robotname']), required=True)
+def read_variable(variable):
+    import pros.serial.devices.vex as vex
+    from pros.serial.ports import DirectPort
+
+    # Get the connected v5 device
+    port = resolve_v5_port(None, 'system')[0]
+    if port == None:
+        return
+    device = vex.V5Device(DirectPort(port))
+    value = device.kv_read(variable)
+    print(value)
+    ui.finalize('readVariable', f'{variable}\'s read value is {value}')
+    
