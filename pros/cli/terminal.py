0c3dd084695b212aab3bb05ac07b6c11fc3ff07a
Neha Sharma <nehasharma.c2021@gmail.com>
2022-07-02 15:29:52 -0400

    Modified imported modules

--- pros/cli/terminal.py
+++ pros/cli/terminal.py
@@ -3,6 +3,7 @@ import signal
 import time
 
 import click
+import sys
 
 import pros.conductor as c
 import pros.serial.devices as devices
@@ -11,7 +12,6 @@ from pros.common.utils import logger
 from .common import default_options, resolve_v5_port, resolve_cortex_port, pros_root
 from pros.serial.ports.v5_wireless_port import V5WirelessPort
 from pros.ga.analytics import analytics
-from pros.common.utils import *
 
 @pros_root
 def terminal_cli():
@@ -42,13 +42,10 @@ def terminal(port: str, backend: str, **kwargs):
     may be preferred when "share" doesn't perform adequately.
 
     Note: share backend is not yet implemented.
-    """
-            
+    """       
     analytics.send("terminal")
     from pros.serial.devices.vex.v5_user_device import V5UserDevice
     from pros.serial.terminal import Terminal
-
-    
     is_v5_user_joystick = False
     if port == 'default':
         project_path = c.Project.find_project(os.getcwd())
@@ -102,7 +99,7 @@ def terminal(port: str, backend: str, **kwargs):
         print(f"arguments in argument dictionary: {kwargs}")
         output_file = kwargs['output']
         print(f'Redirecting Terminal Output to File: {output_file}')
-        sys.stdout = TerminalOutput(f'{output_file}')§
+        sys.stdout = TerminalOutput(f'{output_file}')
     else:
         sys.stdout = sys.__stdout__
 
