6cde856a706b40456edc3b9074a9965ba2473cf4
omegaStag <57331513+omegaStag@users.noreply.github.com>
2023-02-02 09:32:39 -0500

    Fixed making PROS 4 when trying to make PROS 3 project

--- pros/conductor/conductor.py
+++ pros/conductor/conductor.py
@@ -222,6 +222,7 @@ class Conductor(Config):
             # support_kernels for backwards compatibility, but kernel_version should be getting most of the exposure
             kwargs['kernel_version'] = kwargs['supported_kernels'] = project.templates['kernel'].version
         template = self.resolve_template(identifier=identifier, allow_online=download_ok, **kwargs)
+        print("TEMPlATE",template)
         if template is None:
             raise dont_send(
                 InvalidTemplateException(f'Could not find a template satisfying {identifier} for {project.target}'))
