b35ac2b90517c8430afdb2d0c3ab8483500f9b56
ceaglebu <craig.eagleburger@gmail.com>
2023-03-06 22:35:57 -0500

    removed unnecessary exception

--- pros/conductor/project/template_resolution.py
+++ pros/conductor/project/template_resolution.py
@@ -17,8 +17,3 @@ class InvalidTemplateException(Exception):
     def __init__(self, *args, reason: TemplateAction = None):
         self.reason = reason
         super(InvalidTemplateException, self).__init__(*args)
-
-class KernelMismatchException(Exception):
-    def __init__(self, *args, reason: TemplateAction = None):
-        self.reason = reason
-        super(KernelMismatchException, self).__init__(*args)
