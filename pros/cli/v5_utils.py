448e368833fd7740b2f265b2c43d4a0922a68df6
Mayank Patibandla <34776435+mayankpatibandla@users.noreply.github.com>
2024-02-15 19:55:42 -0500

    Explicitly name sv and rv

--- pros/cli/v5_utils.py
+++ pros/cli/v5_utils.py
@@ -291,7 +291,7 @@ def capture(file_name: str, port: str, force: bool = False):
 
     print(f'Saved screen capture to {file_name}')
 
-@v5.command(aliases=['sv', 'set'], short_help='Set a kernel variable on a connected V5 device')
+@v5.command('set-variable', aliases=['sv', 'set'], short_help='Set a kernel variable on a connected V5 device')
 @click.argument('variable', type=click.Choice(['teamnumber', 'robotname']), required=True)
 @click.argument('value', required=True, type=click.STRING, nargs=1)
 @click.argument('port', type=str, default=None, required=False)
@@ -308,7 +308,7 @@ def set_variable(variable, value, port):
     actual_value = device.kv_write(variable, value).decode()
     print(f'Value of \'{variable}\' set to : {actual_value}')
 
-@v5.command(aliases=['rv', 'get'], short_help='Read a kernel variable from a connected V5 device')
+@v5.command('read-variable', aliases=['rv', 'get'], short_help='Read a kernel variable from a connected V5 device')
 @click.argument('variable', type=click.Choice(['teamnumber', 'robotname']), required=True)
 @click.argument('port', type=str, default=None, required=False)
 @default_options
