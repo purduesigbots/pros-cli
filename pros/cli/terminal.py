purduesigbots/pros-cli

commit 8838799725236a114e66a8ff7ea6c72f52a6a21b

Fix R1710

pros/cli/terminal.py
@@ -83,6 +83,7 @@ def terminal(port: str, backend: str, **kwargs):
     else:
         device = devices.vex.V5UserDevice(ser)
     term = Terminal(device, request_banner=kwargs.pop('request_banner', True))
+    return 0
 
     class TerminalOutput:
         def __init__(self, file):

pros/serial/terminal/terminal.py
@@ -148,7 +148,7 @@ elif os.name == 'posix':
                                         [], None)
             if self.pipe_r in ready:
                 os.read(self.pipe_r, 1)
-                return
+                return None
             c = self.enc_stdin.read(1)
             if c == chr(0x7f):
                 c = chr(8)  # map the BS key (which yields DEL) to backspace

.pylintrc
@@ -8,7 +8,6 @@
 # E1120: No value for argument in function call
 # E1123: Unexpected argument in function call
 # C0209: Consider using an f-string
-# R1710: Either all return statements in a function should return an expression, or none of them should
 # W0621: Redefining name from outer scope
 # W0614: Unused import from wildcard import
 # W0401: Wildcard import
@@ -56,7 +55,7 @@
 # R0401: Cyclic import -- Seems to be platform specific
 
 max-line-length = 120
-disable =   C0114, C0115, C0116, R0903, C0415, R0913, W1203, R1729, E1120, E1123, C0209, R1710, W0621,
+disable =   C0114, C0115, C0116, R0903, C0415, R0913, W1203, R1729, E1120, E1123, C0209, W0621,
             W0614, W0401, W1202, W0718, R0914, R1725, C0411, W0237, W0702, W0613,
             R0912, R0911, W0511, R0902, C0412, C0103, C0301, R0915, W1514,
             E1101, W1201,

pros/cli/click_classes.py
@@ -79,7 +79,7 @@ class PROSOption(click.Option):
 
     def get_help_record(self, ctx):
         if hasattr(self, 'hidden') and self.hidden:
-            return
+            return None
         return super().get_help_record(ctx)
 
 class PROSDeprecated(click.Option):

pros/cli/common.py
@@ -71,7 +71,7 @@ def logging_option(f: Union[click.Command, Callable]):
 def logfile_option(f: Union[click.Command, Callable]):
     def callback(ctx: click.Context, param: click.core.Parameter, value: Any):
         if value is None or value[0] is None:
-            return None
+            return
         ctx.ensure_object(dict)
         level = None
         if isinstance(value[1], str):

pros/cli/conductor.py
@@ -77,6 +77,7 @@ def fetch(query: c.BaseTemplate):
     # whether the arguments are for the template or for the depot, so they share them
     logger(__name__).debug(f'Additional depot and template args: {query.metadata}')
     c.Conductor().fetch_template(depot, template, **query.metadata)
+    return 0
 
 
 @conductor.command(context_settings={'ignore_unknown_options': True})

pros/cli/conductor_utils.py
@@ -161,7 +161,7 @@ def purge_template(query: c.BaseTemplate, force):
     beta_templates = cond.resolve_templates(query, allow_online=False, beta=True)
     if len(templates) == 0:
         click.echo('No matching templates were found matching the spec.')
-        return 0
+        return
     t_list = [t.identifier for t in templates] + [t.identifier for t in beta_templates]
     click.echo(f'The following template(s) will be removed {t_list}')
     if len(templates) > 1 and not force:

pros/cli/misc_commands.py
@@ -20,7 +20,7 @@ def upgrade(force_check, no_install):
     with ui.Notification():
         ui.echo('The "pros upgrade" command is currently non-functioning. Did you mean to run "pros c upgrade"?', color='yellow')
 
-    return # Dead code below
+    return None  # Dead code below
 
     analytics.send("upgrade")
     from pros.upgrade import UpgradeManager

commit 5d925daf8cd68164cd8bc66c7b28bfcfe34be570

Fix terminal
Broken in 8838799725236a114e66a8ff7ea6c72f52a6a21b

pros/cli/terminal.py
@@ -83,7 +83,6 @@ def terminal(port: str, backend: str, **kwargs):
     else:
         device = devices.vex.V5UserDevice(ser)
     term = Terminal(device, request_banner=kwargs.pop('request_banner', True))
-    return 0
 
     class TerminalOutput:
         def __init__(self, file):
@@ -118,3 +117,4 @@ def terminal(port: str, backend: str, **kwargs):
         output.end()
     term.join()
     logger(__name__).info('CLI Main Thread Dying')
+    return 0