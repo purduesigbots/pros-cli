d6f15cdf25e8038d333a36b83c649555526e5ab0
JJTheManual <jjyoder35@outlook.com>
2025-10-06 18:04:52 -0400

    Add a new project command

--- pros/conductor/conductor.py
+++ pros/conductor/conductor.py
@@ -2,6 +2,8 @@
 
 import pathlib
 import zipfile
+import requests
+import os
 
 import rich_click as click
 
@@ -33,3 +35,24 @@ def apply(template: pathlib.Path, project: pathlib.Path) -> None:
     """Install a PROS template."""
     with zipfile.ZipFile(template) as f:
         f.extractall(project)
+    
+@conductor.command()
+@click.option(
+    "--project",
+    type=click.Path(
+        exists=True, file_okay=False, dir_okay=True, readable=True, resolve_path=True, path_type=pathlib.Path
+    ),
+    default=pathlib.Path.cwd(),
+)
+def new_project(project: pathlib.Path) -> None:
+    """Create a new PROS project."""
+    url = "https://github.com/purduesigbots/pros-docs/raw/refs/heads/master/v5/_static/releases/kernel@4.2.1.zip"
+    response = requests.get(url)
+
+    with open("kernel.zip", "wb") as file:
+        file.write(response.content)
+
+    with zipfile.ZipFile("kernel.zip") as f:
+        f.extractall(project)
+
+    os.remove("kernel.zip")
